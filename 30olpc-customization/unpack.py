3258a747a6bd82bf97f6c3be640fe9aa9a5d3158
Daniel Drake <dsd@laptop.org>
2009-11-24 15:13:34 +0000

    Finish off customization stick
    Including some minor refactoring of the codebase in order to share
    files between different initramfses.

--- 30olpc-customization/unpack.py
+++ 30olpc-customization/unpack.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.5
+#!/usr/bin/python
 ## Add an '-i' to the #! line to enable the interactive shell.
 ## Remove the -i for deployment, to ensure that an
 ## error in the script doesn't land us in an interactive shell.
@@ -6,8 +6,11 @@
 from __future__ import with_statement
 from os.path import exists, join, isdir, splitext
 from os import fork, _exit, waitpid, setuid, setgid, chmod, umask, walk, system
+import os
+import stat
 from time import sleep
 from traceback import print_exc as trace
+from subprocess import check_call, call
 
 from process import lout, tokenize
 
@@ -49,7 +52,8 @@ def unpack_bundles():
     umask(0022)
 
     for k,v in tgt.iteritems():
-        lout(['/bin/mkdir', '-p', v])
+        if not exists(v):
+            os.makedirs(v)
 
     open('/sysroot/home/olpc/.usb-customizations', 'w').write('1')
 
@@ -72,8 +76,43 @@ def check_adequate_version():
     if exists(version_file):
         assert int(open(version_file, 'r').read(10)) < 1
 
+def get_xo_version():
+    name = open('/sys/class/dmi/id/product_name', 'r').read().strip()
+    version = open('/sys/class/dmi/id/product_version', 'r').read().strip()
+
+    if name != "XO":
+        raise Exception("Not an XO laptop?")
+    if version == "1":
+        return "1"
+    elif version == "1.5":
+        return "1.5"
+    else:
+        raise Exception("Unrecognised version number: %s" % version)
+
+def mknod(dev, maj, min):
+    if not exists(dev):
+        os.mknod(dev, stat.S_IFCHR | 0700, os.makedev(maj, min))
+
+def setup_sysmounts():
+    open("/etc/fstab", "w").close() # create empty file
+    mknod("/dev/null", 1, 3)
+    check_call(['/bin/mount', '-t', 'proc', '/proc', '/proc'])
+    check_call(['/bin/mount', '-t', 'sysfs', '/sys', '/sys'])
+    check_call(['/bin/mount', '-t', 'tmpfs', '-omode=0755', 'udev', '/dev'])
+    mknod("/dev/null", 1, 3)
+    mknod("/dev/ptmx", 5, 2)
+    mknod("/dev/console", 5, 1)
+    mknod("/dev/kmsg", 1, 11)
+
+def run_udev():
+    check_call(['/sbin/udevd', '--daemon'])
+    check_call(['/sbin/udevadm', 'trigger'])
+    check_call(['/sbin/udevadm', 'settle', '--timeout=30'])
+
 def main():
     print "Hello, (deployment people of the) world!"
+
+    setup_sysmounts()
 
     try:
         # unfreeze dcon if it is frozen.
@@ -81,21 +120,29 @@ def main():
     except:
         pass
 
+    run_udev()
+
     # Let the USB stack win the race...
     sleep(5)
 
-    # This next chunk needs some serious generalization.
-    # FIXME: detect XO version from OFW? or DMI?
     mntopts = ""
-    if os.path.exists("/dev/mtdblock0"):
-        mntopts = "-o rw -t jffs2 /dev/mtdblock0"
-    else:
-        # XO-1.5? FIXME
-        mntopts = "-o rw /dev/hda1"
+    xo_ver = get_xo_version()
+    print "Detected XO-%s" % xo_ver
+    if xo_ver == "1":
+        mntopts = "-o rw -t jffs2 mtd0"
+    elif xo_ver == "1.5":
+        if exists("/dev/disk/mmc/mmc2p2"):
+            # XO-1.5 B3 and newer has internal disk here
+            # (mmc2 is wifi on earlier models, we'll never see a disk here)
+            mntopts = "-o rw /dev/disk/mmc/mmc2p2"
+        else:
+            # XO-1.5 B2 and older has internal disk here
+            # XO-1.5 B3 and newer has external SD card here
+            mntopts = "-o rw /dev/disk/mmc/mmc0p2"
 
     with Mounted(path='/mnt/usb', opts='-t vfat -o ro /dev/sda1'):
         check_bundles_dir()
-        with Mounted(path='/sysroot', mntopts):
+        with Mounted(path='/sysroot', opts=mntopts):
             check_adequate_version()
             chmod('/sysroot', 0777)
             chmod('/dev/null', 0777)
@@ -104,7 +151,7 @@ def main():
 
     print 'Bundle installation complete; powering off in five seconds.'
     sleep(5)
-    system("poweroff -f")
+    call(["/sbin/poweroff", "-f"])
     return
 
 if __name__ == '__main__': main()
