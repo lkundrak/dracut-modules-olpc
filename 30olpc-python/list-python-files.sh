1000cab89a29207dc845d16a7b789535afe28857
Daniel Drake <dsd@laptop.org>
2010-08-16 18:46:21 -0600

    Fix bash regexp syntax
    Newer versions of bash are pickier about this.

--- 30olpc-python/list-python-files.sh
+++ 30olpc-python/list-python-files.sh
@@ -13,22 +13,22 @@ for line in $lines; do
 	[ -d "$file" ] && continue
 
 	# documentation
-	[[ "$file" =~ '^/usr/share/(doc|man)' ]] && continue
+	[[ $file =~ ^/usr/share/(doc|man) ]] && continue
 
 	# stuff we don't need
-	[[ "$file" =~ "^/usr/lib/python2\..*/(idlelib|distutils|compiler|email|xml|multiprocessing|json|ctypes|bsddb|hotshot|logging|wsgiref|curses|plat-linux2|sqlite3)/" ]] && continue
+	[[ $file =~ ^/usr/lib/python2\..*/(idlelib|distutils|compiler|email|xml|multiprocessing|json|ctypes|bsddb|hotshot|logging|wsgiref|curses|plat-linux2|sqlite3)/ ]] && continue
 
 	# dynload stuff
-	[[ "$file" =~ "^/usr/lib/python2\..*/lib-dynload/(pyexpat|nismodule|dbm|cryptmodule|bz2|_sqlite3|_hashlib|_cursesmodule|_curses_panel|_ctypes|_bsddb|readline|gdbmmodule|_ssl|unicodedata|_codecs_..)\.so" ]] && continue
+	[[ $file =~ ^/usr/lib/python2\..*/lib-dynload/(pyexpat|nismodule|dbm|cryptmodule|bz2|_sqlite3|_hashlib|_cursesmodule|_curses_panel|_ctypes|_bsddb|readline|gdbmmodule|_ssl|unicodedata|_codecs_..)\.so ]] && continue
 
 	# pyo objects
-	[[ "$file" =~ "\.py[co]$" ]] && continue
+	[[ $file =~ \.py[co]$ ]] && continue
 
 	# pydoc
 	[ "$file" == "/usr/bin/pydoc" ] && continue
 
 	# misc
-	[[ "$file" =~ "(regen|README|logfix|pydoc_topics.py|decimal.py|doctest.py|pydoc.py|python-config|python2\..*-config)$" ]] && continue
+	[[ $file =~ (regen|README|logfix|pydoc_topics.py|decimal.py|doctest.py|pydoc.py|python-config|python2\..*-config)$ ]] && continue
 
 	echo $file
 done
