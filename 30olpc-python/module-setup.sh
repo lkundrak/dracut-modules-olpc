7c0d3fb27731bf32ca74163f8276fd44659631a8
Daniel Drake <dsd@laptop.org>
2011-12-30 08:52:44 -0600

    olpc-python: don't mangle IFS while calling inst
    This was preventing the installation of
    /usr/include/python2.7/pyconfig-32.h
    which is required for python to work.

--- 30olpc-python/module-setup.sh
+++ 30olpc-python/module-setup.sh
@@ -5,10 +5,14 @@ check() {
 
 install() {
 	old_ifs=$IFS
-	IFS="
+	new_ifs="
 	"
+	IFS=$new_ifs
 	for line in $(<"$moddir"/python-contents.txt); do
+		# Must restore regular IFS inside loop as it is used by inst
+		IFS=$old_ifs
 		inst $line
+		IFS=$new_ifs
 	done
 	IFS=$old_ifs
 }
