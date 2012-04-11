0aa5856ad8af016392e1028cc02782504f2964f9
Daniel Drake <dsd@laptop.org>
2012-04-11 16:20:17 -0600

    olpc-common: update network interface naming config from olpc-utils
    Sync in the new, better way of grabbing eth0 exclusively for our
    internal wifi.

--- 30olpc-common/module-setup.sh
+++ 30olpc-common/module-setup.sh
@@ -7,4 +7,5 @@ install() {
 	mkdir -p "$initdir"/etc/udev/rules.d
 	inst "$moddir"/olpc-sd.rules /etc/udev/rules.d/10-olpc-sd.rules
 	inst "$moddir"/olpc-net.rules /etc/udev/rules.d/10-olpc-net.rules
+	inst "$moddir"/olpc_eth_namer /lib/udev/olpc_eth_namer
 }
