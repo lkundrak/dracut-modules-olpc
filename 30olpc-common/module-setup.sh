99e235f2ae9019b113d004f243882eab9b9de3a4
Daniel Drake <dsd@laptop.org>
2012-06-20 10:17:05 -0600

    olpc-common: add emergency handler to prevent debug shell
    To avoid any possibility of executing the debug shell, add an emergency
    handler to prevent this.
    
    Debug shell can be enabled by developers with the olpc.emergency boot
    parameter.

--- 30olpc-common/module-setup.sh
+++ 30olpc-common/module-setup.sh
@@ -8,4 +8,6 @@ install() {
 	inst "$moddir"/olpc-sd.rules /etc/udev/rules.d/10-olpc-sd.rules
 	inst "$moddir"/olpc-net.rules /etc/udev/rules.d/10-olpc-net.rules
 	inst "$moddir"/olpc_eth_namer /lib/udev/olpc_eth_namer
+
+	inst_hook emergency 10 "$moddir"/olpc-common-emergency.sh
 }
