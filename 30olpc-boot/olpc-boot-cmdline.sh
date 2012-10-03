432a6f05e91c71bfb44f2e4d4ced00d02e057669
Daniel Drake <dsd@laptop.org>
2012-10-03 13:50:31 -0600

    olpc-boot: move XO-1.75 bootpath match for compatibility with OFW hack
    OFW now scans the initramfs for a certain string in order to present
    a bootpath value that is backwards-compatible with existing OS releases.
    
    As we aren't yet ready to embrace the new DT path, adjust the code
    to continue meeting this string match.

--- 30olpc-boot/olpc-boot-cmdline.sh
+++ 30olpc-boot/olpc-boot-cmdline.sh
@@ -19,15 +19,20 @@ if [ -z "$root" ]; then
 	# XXX: unpartitioned XO-1.5 not supported
 	# XXX: unpartitioned USB not supported
 	# XXX: might get confused if more than 1 USB disk is plugged in
+
+	# $bootpath refers to a DT node, which assumes that new firmware versions
+	# will not change DT layout/naming. However, change has happened, in the
+	# work to move to DT-only upstream kernels on ARM, but we have shipped
+	# non-DT kernels/firmwares for XO-1.75.
+	#
+	# As such, new XO-1.75 firmware versions scan the initramfs and look for
+	# the string "$bootpath in\n\t\t/sd@", and if found, hack the bootpath
+	# node to have the old "sd@" value.
+	#
+	# This initramfs code must be maintained to continue meeting that match
+	# until we are ready to handle DT-based bootpaths for XO-1.75, which
+	# requires some attention to do it right.
 	case $bootpath in
-		/sd/sdhci@d4280000/disk:*) # device tree
-			# FIXME: XO-4 ext SD, might break for XO-1.75
-			root="/dev/disk/mmc/mmc2p2"
-			;;
-		/sd/sdhci@d4281000/disk:*) # device tree
-			# FIXME: XO-4 eMMC, might break for XO-1.75
-			root="/dev/disk/mmc/mmc1p2"
-			;;
 		/sd@d4280000/disk@?:*) # XO-1.75
 			# extract the bus number (from disk@NUM) and decrement by 1 to
 			# correlate with linux device
@@ -35,6 +40,14 @@ if [ -z "$root" ]; then
 			tmp=${tmp%%:*}
 			tmp=$((tmp - 1))
 			root="/dev/disk/mmc/mmc${tmp}p2"
+			;;
+		/sd/sdhci@d4280000/disk:*) # device tree
+			# FIXME: XO-4 ext SD, might break for XO-1.75
+			root="/dev/disk/mmc/mmc2p2"
+			;;
+		/sd/sdhci@d4281000/disk:*) # device tree
+			# FIXME: XO-4 eMMC, might break for XO-1.75
+			root="/dev/disk/mmc/mmc1p2"
 			;;
 		/pci/sd@c/disk@?:*) # XO-1.5 SD card
 			# Same calculation as XO-1.75
