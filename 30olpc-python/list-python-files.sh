7269c474fe72f141fa0d6d096ef580cde0ad5e37
Daniel Drake <dsd@laptop.org>
2013-03-15 16:38:22 -0600

    Update Python file list
    and add the hashlib module, replacing the individual hashing libraries
    that we used to ship, requested by Esteban Bordón.

--- 30olpc-python/list-python-files.sh
+++ 30olpc-python/list-python-files.sh
@@ -19,7 +19,7 @@ for line in $lines; do
 	[[ $file =~ ^/usr/lib/python2\..*/(idlelib|distutils|compiler|email|xml|multiprocessing|json|bsddb|hotshot|logging|wsgiref|curses|plat-linux2|sqlite3|unittest|lib2to3)/ ]] && continue
 
 	# dynload stuff
-	[[ $file =~ ^/usr/lib/python2\..*/lib-dynload/(pyexpat|nismodule|dbm|cryptmodule|bz2|_sqlite3|_hashlib|_cursesmodule|_curses_panel|_bsddb|readline|gdbmmodule|_ssl|unicodedata|_codecs_..)\.so ]] && continue
+	[[ $file =~ ^/usr/lib/python2\..*/lib-dynload/(pyexpat|nismodule|dbm|cryptmodule|bz2|_sqlite3|_cursesmodule|_curses_panel|_bsddb|readline|gdbmmodule|_ssl|unicodedata|_codecs_..)\.so ]] && continue
 
 	# pyo objects
 	[[ $file =~ \.py[co]$ ]] && continue
